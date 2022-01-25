5ddb0a272bc1fdfc4b2eb6c63fe4eb0e0cc07cdf
ekaterina_pchelenko <e.pchelenko@gmail.com>
2022-01-25 19:09:25 +0500

    task 1

---
+++ client.py
@@ -0,0 +1,54 @@
+import sys
+import json
+import socket
+import time
+from common.variables import ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, \
+    RESPONSE, ERROR, DEFAULT_IP_ADDRESS, DEFAULT_PORT
+from common.utils import get_message, send_message
+
+
+def create_presence(username='Guest'):
+    info = {
+        ACTION: PRESENCE,
+        TIME: time.time(),
+        USER: {
+            ACCOUNT_NAME: username
+        }
+    }
+    return info
+
+
+def process_ans(message):
+    if RESPONSE in message:
+        if message[RESPONSE] == 200:
+            return '200 : OK'
+        return f'400 : {message[ERROR]}'
+    raise ValueError
+
+
+def main():
+    try:
+        client_address = sys.argv[1]
+        client_port = int(sys.argv[2])
+        if client_port < 1024 or client_port > 65535:
+            raise ValueError
+    except IndexError:
+        client_address = DEFAULT_IP_ADDRESS
+        client_port = DEFAULT_PORT
+    except ValueError:
+        print('В качестве порта может быть указано только число в диапазоне от 1024 до 65535.')
+        sys.exit(1)
+
+    transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    transport.connect((client_address, client_port))
+    message = create_presence()
+    send_message(transport, message)
+    try:
+        answer = process_ans(get_message(transport))
+        print(answer)
+    except (ValueError, json.JSONDecodeError):
+        print('Не удалось декодировать сообщение сервера.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ server.py
@@ -0,0 +1,65 @@
+import socket
+import sys
+import json
+from common.variables import ACTION, ACCOUNT_NAME, RESPONSE, MAX_CONNECTIONS, \
+    PRESENCE, TIME, USER, ERROR, DEFAULT_PORT
+from common.utils import get_message, send_message
+
+
+def process_client_message(message):
+    if ACTION in message and message[ACTION] == PRESENCE and TIME in message \
+            and USER in message and message[USER][ACCOUNT_NAME] == 'Guest':
+        return {RESPONSE: 200}
+    return {
+        RESPONSE: 400,
+        ERROR: 'Bad Request'
+    }
+
+
+def main():
+    try:
+        if '-p' in sys.argv:
+            server_port = int(sys.argv[sys.argv.index('-p') + 1])
+        else:
+            server_port = DEFAULT_PORT
+        if server_port < 1024 or server_port > 65535:
+            raise ValueError
+    except IndexError:
+        print('Укажите номер порта после параметра -\'p\'.')
+        sys.exit(1)
+    except ValueError:
+        print(
+            'Номер порта может быть только от 1024 до 65535.')
+        sys.exit(1)
+
+    try:
+        if '-a' in sys.argv:
+            server_address = sys.argv[sys.argv.index('-a') + 1]
+        else:
+            server_address = ''
+
+    except IndexError:
+        print(
+            'После параметра \'a\'- необходимо указать адрес, который будет слушать сервер.')
+        sys.exit(1)
+
+    transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    transport.bind((server_address, server_port))
+
+    transport.listen(MAX_CONNECTIONS)
+
+    while True:
+        client, client_address = transport.accept()
+        try:
+            client_message = get_message(client)
+            print(client_message)
+            response = process_client_message(client_message)
+            send_message(client, response)
+            client.close()
+        except (ValueError, json.JSONDecodeError):
+            print('Принято некорретное сообщение от клиента.')
+            client.close()
+
+
+if __name__ == '__main__':
+    main()
